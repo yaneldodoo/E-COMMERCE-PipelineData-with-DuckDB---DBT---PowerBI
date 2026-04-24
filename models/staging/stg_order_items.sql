88239590e4d95f09b91d30dd2be8e65541602ed1
yaneldodoo <yaneldodoo@gmail.com>
2026-04-24 11:15:22 +0200

    Initial commit dbt project

---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,14 @@
+-- models/staging/stg_order_items.sql
+  
+  WITH source AS (
+      SELECT * FROM {{ source('raw', 'raw_order_items') }}
+  )
+  
+  SELECT
+      order_id,
+      product_id,
+      seller_id,
+      CAST(price             AS DECIMAL(10,2)) AS price,
+      CAST(shipping_charges  AS DECIMAL(10,2)) AS shipping_charges,
+      price + shipping_charges                 AS total_item_amount
+  FROM source
