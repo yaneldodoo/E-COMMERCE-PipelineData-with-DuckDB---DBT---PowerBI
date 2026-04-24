b5b542c658a96112ab26d46f1fb71ebae7d78c62
yaneldodoo <yaneldodoo@gmail.com>
2026-04-24 13:41:29 +0200

    stg_customers

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,11 @@
+with source as (
+    select * from {{ source('raw', 'raw_customers') }}
+), 
+renamed as (
+    select customer_id, 
+    cast(customer_city as varchar) as  city,
+    cast(customer_state as varchar) as state,
+from source
+),
+select * from renamed 
+
