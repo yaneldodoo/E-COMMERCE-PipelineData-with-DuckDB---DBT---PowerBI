3b4b3cf8ac2fbd3e8e4b57a7f8fa234384dbfb45
yaneldodoo <yaneldodoo@gmail.com>
2026-04-25 10:27:31 +0200

    update

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -2,9 +2,9 @@ with source as (
     select * from {{ source('raw', 'raw_products') }}
 ), 
 renamed as (
-    select product_id, 
-    cast(product_name as varchar) as  nameproduct,
-    cast(product_category as varchar) as category,
+    select 
+    cast(product_id as varchar) as  idproduct,
+    cast(product_category_name as varchar) as category,
     from source 
 )
 select * from renamed
